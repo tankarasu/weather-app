6f6254cfe35fbc52d2fac8a1beca574354fde6cc
Tan KARASU <karasutan@gmail.com>
2020-06-25 21:07:19 +0200

    premier jet ok et propre

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./style.css";
 import "../assets/img/localisation.png";
 import "../assets/img/weather.png";
+import "../assets/img/nodeicon.png";
+import "../assets/img/weatherapp.png";
+import "../assets/img/IP.png";
+import "../assets/img/ville.png";
 import "../assets/img/me.jpg";
 import '../assets/js/app.js'
